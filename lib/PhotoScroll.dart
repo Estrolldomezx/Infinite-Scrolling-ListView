e0ebb526a3265492982210fdec2a1ac9db6e1b37
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-07-12 19:50:11 +0700

    create state

---
+++ lib/PhotoScroll.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PhotoScroll extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PhotoScroll_State();
+  }
+}
+
+class _PhotoScroll_State extends State<PhotoScroll> {
+  
+}
+
